afc288f0a3c431e70d658c43f96ffbea455853ce
Blindffgm <150510220+Blindffgm@users.noreply.github.com>
2024-09-02 07:52:42 +0300

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Playing VALORANT"];
+const statusMessages = ["Jrp Discord Bot","Playing JEFFERSON ROLEPLAY"];
 
 
 let currentIndex = 0;
